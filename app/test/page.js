2038721254df466800f681dbeac69d4232c788dd
saurabh4323 <saurabhiitr01@gmail.com>
2025-04-19 18:01:29 +0530

    rr

--- app/test/page.js
+++ app/test/page.js
@@ -1,6 +1,14 @@
 "use client";
+import { GradientButton, MinimalButton, NeonButton } from "devjunkie";
 import React from "react";
-import { GradientButton } from "devjunkie";
+// import CoolButtons, { GradientButton } from "./coolButton";
+// import { GradientButton } from "devjunkie";
 export default function page() {
-  return <div></div>;
+  return (
+    <div>
+      <GradientButton>Click me!</GradientButton>
+      <MinimalButton>Click me!</MinimalButton>
+      <NeonButton>Click me!</NeonButton>
+    </div>
+  );
 }
